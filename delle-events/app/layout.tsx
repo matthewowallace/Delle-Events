469e765d30334fd01dc9539cb65e83d82c988232
Matthew <wallaceomatthew@gmail.com>
2025-11-27 07:38:20 -0600

    added Lightrays with Navbar

--- delle-events/app/layout.tsx
+++ delle-events/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from '@/components/LightRays';
+import Navbar from "@/components/Navbar";
 
 const schibsted_Grotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -27,7 +29,24 @@ export default function RootLayout({
       <body
         className={`${schibsted_Grotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
-        {children}
+      <div className={"absolute inset-0 top-0 z-[-1] min-h-screen"}>
+          <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#F5366B"
+              raysSpeed={1.5}
+              lightSpread={1.3}
+              rayLength={1.5}
+              followMouse={true}
+              mouseInfluence={0.1}
+              noiseAmount={0.1}
+              distortion={0.05}
+              className="custom-rays"
+          />
+      </div>
+      <Navbar/>
+          <main>
+            {children}
+          </main>
       </body>
     </html>
   );
--- delle-events/app/page.tsx
+++ delle-events/app/page.tsx
@@ -1,10 +1,32 @@
+'use client'
+
 import Image from "next/image";
+import { IconGitBranch } from "@tabler/icons-react"
+import { Button } from "@/components/ui/button"
+import {AnimatedCompass, CompassIcon} from "@/components/CompassIcon";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1>Welcome to Next Js</h1>
-    </div>
+    <section className="flex justify-center px-4 py-20">
+        <div className={"max-w-2xl text-center"}>
+        <h1  className="text-center">Connect Through Code</h1>
+        <p className={"mt-5 text-sm pb-10"}>Explore a curated ecosystem of developer-focused events. Whether you're hosting or attending, our platform streamlines event discovery, management, and collaboration for the modern tech community.</p>
+        <Button variant="outline" size="lg" onClick={() => alert("New Branch")} id={"explore-btn"} >
+            <a href="#events">
+                <span>Explore Events</span>
+                <CompassIcon size={24} className="inline-block justify-center" />
+            </a>
+        </Button>
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+                <ul className="events">
+                    {[1,2,3,4,5].map((event)=>(
+                        <li key={event}></li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    </section>
   );
 }
 
